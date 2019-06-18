d1b555d7245dc360c3b5f18aed9f57bc30265d3a
Akshay Parashar <akshay.ahmedabad@gmail.com>
2019-06-18 16:26:52 +0530

    Add files via upload
    Update with Methods

--- crop_class.py
+++ crop_class.py
@@ -1,4 +1,5 @@
 class Crop:
+    # Constructor
     def __init__(self, growth_rate, light_need, water_need):
         self._growth = 0
         self._days_growing = 0
@@ -8,11 +9,19 @@ class Crop:
         self._status = "Seed"
         self._type = "Generic"
 
+    def needs(self):
+        # Return the data in a dictionary format (Key: Value pair)
+        return {'light need': self._light_need, 'water need': self._water_need}
+
+    # Report current status of Crop
+    def report(self):
+        return {'type': self._type, 'status': self._status, 'growth': self._growth, 'days growing': self._days_growing}
+
+
 def main():
     new_crop = Crop(1, 4, 3)
-    print(new_crop._status)
-    print(new_crop._type)
-    print(new_crop._growth)
+    print(new_crop.needs()["water need"])
+    print(new_crop.report()["status"])
 
 if __name__ == "__main__":
     main()
